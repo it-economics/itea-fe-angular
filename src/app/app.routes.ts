76c3c7dc9ebefaa7aacfa757bb7f6e3cbcae49e8
doennyl <dliebmann@it-economics.de>
2024-03-10 14:01:17 +0100

    basic setup: adds first page structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: '**', component: ProductsComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'product', component: AppComponent }, //TODO @Danny
+  { path: 'about', component: AboutUsComponent },
+  { path: 'imprint', component: ImprintComponent },
+];
