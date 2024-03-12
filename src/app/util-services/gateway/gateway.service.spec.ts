3c1f7d2f5e6377cc4f6de0d1abba801468e5e9e8
doennyl <dliebmann@it-economics.de>
2024-03-12 11:38:30 +0100

    impl: adds gateway files

---
+++ src/app/util-services/gateway/gateway.service.spec.ts
@@ -0,0 +1,74 @@
+import { GatewayService } from './gateway.service';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { of } from 'rxjs';
+
+describe('GatewayService', () => {
+    let service: GatewayService<void>;
+    let httpMock: jasmine.SpyObj<HttpClient>;
+
+    beforeEach(() => {
+        httpMock = jasmine.createSpyObj('HttpClient', ['get', 'post', 'put', 'patch', 'delete']);
+        service = new GatewayService(httpMock);
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should send get http request', (done) => {
+        httpMock.get.and.returnValue(of('get'));
+        service.get('test').subscribe((value) => {
+            expect(value).toEqual('get');
+            done();
+        });
+    });
+
+    it('should send post http request', (done) => {
+        const payload = { id: '1', name: 'post' };
+        httpMock.post.and.returnValue(of(payload));
+        service.post('post', payload).subscribe((value) => {
+            expect(value).toEqual(payload);
+            done();
+        });
+    });
+
+    it('should send put http request', (done) => {
+        const payload = { id: '1', name: 'put' };
+        httpMock.put.and.returnValue(of(payload));
+        service.put('put', payload).subscribe((value) => {
+            expect(value).toEqual(payload);
+            done();
+        });
+    });
+    it('should send patch http request', (done) => {
+        const payload = { id: '1', name: 'patch' };
+        httpMock.patch.and.returnValue(of(payload));
+        service.patch('patch', payload).subscribe((value) => {
+            expect(value).toEqual(payload);
+            done();
+        });
+    });
+
+    it('deletes http request without params', (done) => {
+        const payload = { id: '1', name: 'delete' };
+        httpMock.delete.and.returnValue(of(payload));
+        service.delete('delete').subscribe((value) => {
+            expect(value).toEqual(payload);
+            done();
+        });
+        expect(httpMock.delete).toHaveBeenCalledOnceWith('delete');
+    });
+
+    it('should send delete http request with params', (done) => {
+        const httpParams = new HttpParams();
+        const payload = { id: '1', name: 'delete' };
+        httpMock.delete.and.returnValue(of(payload));
+        service.delete('delete', httpParams).subscribe((value) => {
+            expect(value).toEqual(payload);
+            done();
+        });
+        expect(httpMock.delete).toHaveBeenCalledOnceWith('delete', {
+            params: httpParams,
+        });
+    });
+});
---
+++ src/app/util-services/gateway/gateway.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root',
+})
+// TODO @Danny remove
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export class GatewayService<T> {
+    constructor(private readonly _http: HttpClient) {}
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    get<T>(path: string, params?: HttpParams, httpResponse?: 'response', type?: any): Observable<any> {
+        return httpResponse
+            ? this._http.get(path, {
+                  observe: httpResponse,
+                  params,
+                  responseType: type,
+              })
+            : this._http.get<T>(path, { params });
+    }
+
+    post<T>(path: string, payload: T | null, options?: object): Observable<T> {
+        return this._http.post<T>(path, payload, options);
+    }
+
+    put<T>(path: string, payload: T): Observable<T> {
+        return this._http.put<T>(path, payload);
+    }
+
+    patch<T>(path: string, payload: object): Observable<T> {
+        return this._http.patch<T>(path, payload);
+    }
+
+    delete<T>(path: string, params?: HttpParams): Observable<T> {
+        if (params) {
+            return this._http.delete<T>(path, { params });
+        } else {
+            return this._http.delete<T>(path);
+        }
+    }
+}
