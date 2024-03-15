8902cfab4882a636e904fcfe18e72f8c6d6116c0
doennyl <dliebmann@it-economics.de>
2024-03-15 09:30:13 +0100

    impl: adds product button

--- src/app/products/product-tile/product-tile.component.ts
+++ src/app/products/product-tile/product-tile.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { Product } from '@models';
 import { CurrencyPipe } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector: 'app-product-tile',
     standalone: true,
-    imports: [CurrencyPipe, MatCardModule],
+    imports: [CurrencyPipe, MatCardModule, MatIconModule],
     templateUrl: './product-tile.component.html',
     styleUrl: './product-tile.component.scss',
 })
