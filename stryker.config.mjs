e0ee1dcfe2c711d88f9ceca9678c0b6d5dd43e45
doennyl <danny.liebmann@soprasteria.com>
2024-06-20 10:43:24 +0200

    adds stryker

---
+++ stryker.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+/** @type {import('@stryker-mutator/api/core').PartialStrykerOptions} */
+const config = {
+    _comment:
+        "This config was generated using 'stryker init'. Please see the guide for more information: https://stryker-mutator.io/docs/stryker-js/guides/angular",
+    mutate: [
+      'src/**/*.ts',
+      '!src/**/*.spec.ts',
+      '!src/environments/*',
+      '!src/app/assets/stubs/*',
+      '!src/app/*.ts'
+    ],
+    testRunner: 'karma',
+    karma: {
+        configFile: 'karma.conf.js',
+        projectType: 'angular-cli',
+        config: {
+            browsers: ['ChromeHeadless'],
+        },
+    },
+    reporters: ['progress', 'clear-text', 'html'],
+    concurrency: 4,
+    concurrency_comment: 'Recommended to use about half of your available cores when running stryker with angular',
+    coverageAnalysis: 'perTest',
+};
+export default config;
