68157e7dad33f67f9ca7381ee90e13ddce875582
SCHMID Stefanie <stefanie.schmid@soprasteria.com>
2025-07-11 09:52:12 +0200

    Migrate Angular 18 to 19

--- src/app/products/product-tile/product-tile.component.ts
+++ src/app/products/product-tile/product-tile.component.ts
@@ -1,14 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { CurrencyPipe } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 import { Product } from '@models/product';
 
 @Component({
     selector: 'app-product-tile',
     standalone: true,
-    imports: [CurrencyPipe, MatCardModule, MatIconModule, RouterLink],
+    // imports: [CurrencyPipe, MatCardModule, MatIconModule, RouterLink],
+    imports: [CurrencyPipe, RouterLink],
     templateUrl: './product-tile.component.html',
     styleUrl: './product-tile.component.scss',
 })
