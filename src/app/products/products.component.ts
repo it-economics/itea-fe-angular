73cd031a3ecaa9a0a71e540a5acc1d996a902d78
dliebmann <danny.liebmann@soprasteria.com>
2025-07-16 19:06:34 +0200

    adds simple product filter

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -7,44 +7,46 @@ import { ProductsApiService } from '@services/http-services/products/products-ap
 import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-products',
-  standalone: true,
-  imports: [NgForOf, ProductTileComponent, FormsModule],
-  templateUrl: './products.component.html',
-  styleUrl: './products.component.scss',
+    selector: 'app-products',
+    standalone: true,
+    imports: [NgForOf, ProductTileComponent, FormsModule],
+    templateUrl: './products.component.html',
+    styleUrl: './products.component.scss',
 })
 export class ProductsComponent implements OnInit, OnDestroy {
-  private _destroy$: Subject<void> = new Subject<void>();
-  private _productApiService = inject(ProductsApiService);
-  products: Product[] = [];
-  filteredProducts: Product[] = [];
-  searchTerm: string;
+    private _destroy$: Subject<void> = new Subject<void>();
+    private _productApiService = inject(ProductsApiService);
+    products: Product[] = [];
+    filteredProducts: Product[] = [];
+    searchTerm: string;
 
-  ngOnInit() {
-    this._productApiService
-      .getProducts()
-      .pipe(takeUntil(this._destroy$))
-      .subscribe((products) => {
-        this.products = products;
-        this.filteredProducts = products;
-      });
-  }
-
-  ngOnDestroy() {
-    this._destroy$.next();
-    this._destroy$.complete();
-  }
-
-  onInputChange(value: string): void {
-    if (!this.products) return;
-    if (!value || value.length < 3) {
-      this.filteredProducts = this.products;
-    } else {
-      this.filteredProducts = this.products.filter(product => this.nomaliseText(product.name).includes(this.nomaliseText(value)));
+    ngOnInit() {
+        this._productApiService
+            .getProducts()
+            .pipe(takeUntil(this._destroy$))
+            .subscribe((products) => {
+                this.products = products;
+                this.filteredProducts = products;
+            });
     }
-  }
 
-  private nomaliseText(text: string): string {
-    return text.trim().toUpperCase();
-  }
+    ngOnDestroy() {
+        this._destroy$.next();
+        this._destroy$.complete();
+    }
+
+    onInputChange(value: string): void {
+        if (!this.products) return;
+        if (!value || value.length < 3) {
+            this.filteredProducts = this.products;
+        } else {
+            this.filteredProducts = this.products.filter((product) =>
+                this.nomaliseText(product.name).includes(this.nomaliseText(value))
+            );
+        }
+    }
+
+    private nomaliseText(text: string): string {
+        return text.trim().toUpperCase();
+    }
 }
