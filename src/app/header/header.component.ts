1836dd227330a296b1772a2b89966d311dd2c88a
SCHMID Stefanie <stefanie.schmid@soprasteria.com>
2025-07-16 08:53:21 +0200

    adjust styling

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [RouterLink],
+    imports: [RouterLink, MatToolbarModule, MatTabsModule, MatButtonModule, MatIconModule],
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss',
 })
