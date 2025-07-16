414890997ded09c24050e37387779280e063b8db
dliebmann <danny.liebmann@soprasteria.com>
2025-07-16 19:06:22 +0200

    adds simple product filter

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -4,30 +4,47 @@ import { NgForOf } from '@angular/common';
 import { ProductTileComponent } from './product-tile/product-tile.component';
 import { Product } from '@models/product';
 import { ProductsApiService } from '@services/http-services/products/products-api.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-    selector: 'app-products',
-    standalone: true,
-    imports: [NgForOf, ProductTileComponent],
-    templateUrl: './products.component.html',
-    styleUrl: './products.component.scss',
+  selector: 'app-products',
+  standalone: true,
+  imports: [NgForOf, ProductTileComponent, FormsModule],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.scss',
 })
 export class ProductsComponent implements OnInit, OnDestroy {
-    products: Product[] = [];
-    private _destroy$: Subject<void> = new Subject<void>();
-    private _productApiService = inject(ProductsApiService);
+  private _destroy$: Subject<void> = new Subject<void>();
+  private _productApiService = inject(ProductsApiService);
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
+  searchTerm: string;
 
-    ngOnInit() {
-        this._productApiService
-            .getProducts()
-            .pipe(takeUntil(this._destroy$))
-            .subscribe((products) => {
-                this.products = products;
-            });
-    }
+  ngOnInit() {
+    this._productApiService
+      .getProducts()
+      .pipe(takeUntil(this._destroy$))
+      .subscribe((products) => {
+        this.products = products;
+        this.filteredProducts = products;
+      });
+  }
 
-    ngOnDestroy() {
-        this._destroy$.next();
-        this._destroy$.complete();
+  ngOnDestroy() {
+    this._destroy$.next();
+    this._destroy$.complete();
+  }
+
+  onInputChange(value: string): void {
+    if (!this.products) return;
+    if (!value || value.length < 3) {
+      this.filteredProducts = this.products;
+    } else {
+      this.filteredProducts = this.products.filter(product => this.nomaliseText(product.name).includes(this.nomaliseText(value)));
     }
+  }
+
+  private nomaliseText(text: string): string {
+    return text.trim().toUpperCase();
+  }
 }
