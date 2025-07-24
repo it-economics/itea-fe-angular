e2abf998c385ee3ecc8fb55abd3c5e5d51dbd0a4
SCHMID Stefanie <stefanie.schmid@soprasteria.com>
2025-07-24 14:08:15 +0200

    use json-server for mocks in dev mode

--- src/app/services/http-services/products/products-api.service.ts
+++ src/app/services/http-services/products/products-api.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable, inject, isDevMode } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { ProductResponse } from '@services/http-services/products/product-response';
 import { ProductMapper } from '@services/http-services/products/product-mapper';
@@ -13,14 +13,30 @@ export class ProductsApiService {
     private readonly _gateway = inject(GatewayService<Product>);
 
     getProducts(): Observable<Product[]> {
+        if (isDevMode()) {
+            return this.getMockProducts();
+        }
+
         return this._gateway
             .get<ProductsResponse>(`products`)
             .pipe(map((response) => ProductMapper.fromProductsResponse(response)));
     }
 
     getProduct(id: number): Observable<Product> {
+        if (isDevMode()) {
+            return this.getMockProduct(id);
+        }
+
         return this._gateway
             .get<ProductResponse>(`product/${id}`)
             .pipe(map((response) => ProductMapper.fromProductResponse(response)));
+    }
+
+    private getMockProducts(): Observable<Product[]> {
+        return this._gateway.get<ProductsResponse>(`products`);
+    }
+
+    private getMockProduct(id: number): Observable<Product> {
+        return this._gateway.get<ProductResponse>(`products/${id}`);
     }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    SERVICE_URL: 'http://localhost:3000/',
+    CDN_IMAGE_URL: 'http://localhost:3000/',
+};
